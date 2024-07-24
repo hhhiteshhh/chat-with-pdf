f74bfcc9df2c60b4ca284b63b7f0402beaf26879
HiteshKumar <hitesh@pharmaquant.org>
2024-07-24 18:33:59 +0530

    basic ui build

---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,13 @@
+import FileUploader from "@/components/FileUploader";
+
+export const dynamic = "force-dynamic";
+
+function UploadPage() {
+  return (
+    <div>
+      <FileUploader />
+    </div>
+  );
+}
+
+export default UploadPage;
